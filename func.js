965642ad4daff541a04c99f5298925c29962c4b0
Abdul-b7 <144189368+Abdul-b7@users.noreply.github.com>
2023-09-06 02:26:52 +0100

    Add files via upload

---
+++ func.js
@@ -0,0 +1,60 @@
+// var gift_item_2 = document.getElementById("gift_item_2");
+function bgchange1() {
+    var eeee1 = document.getElementById("gift_item_1")
+        eeee1.style.backgroundImage = "url('imgs/gift-1.gif')"
+        eeee1.style.backgroundSize = "cover"
+        eeee1.style.transition = '3s'
+//   document.getElementById("gift_item_1").style.backgroundImage =
+//     "url('imgs/IMG_8121.jpeg')";
+}
+function bgchange2() {
+//   document.getElementById("gift_item_2").style.backgroundImage =
+//     "url('imgs/IMG_8155~2.jpeg')";
+    var eeee = document.getElementById("gift_item_2")
+        eeee.style.backgroundImage = "url('imgs/gift-2.gif')"
+        eeee.style.backgroundSize = "cover"
+        eeee.style.transition = '3s'
+        // eeee.style.borderColor = 'red'
+        // eeee.style.backgroundColor = 'red'
+        // eeee.style.backgroundPosition = "center"
+}
+function bgchange3() {
+//   document.getElementById("gift-item-2").style.backgroundImage =
+//     "url('imgs/IMG_8120.jpeg')";
+    var eeee3 = document.getElementById("gift-item-2")
+        eeee3.style.backgroundImage = "url('imgs/gift-3.jpg')"
+        eeee3.style.backgroundSize = "cover"
+        eeee3.style.transition = '3s'
+}
+
+function redbor(){
+    var redcolor = document.getElementById("main-t")
+    redcolor.style.backgroundImage = "url('imgs/giphy1.webp')"
+    redcolor.style.backgroundPosition = "center"
+    redcolor.style.backgroundSize = "cover"
+    redcolor.style.transition = '3s'
+    // var borderchange = document.getElementById("borchan")
+    // borderchange.style.borderColor = "#70b5fa"
+}
+function bluebor(){
+    var bluecolor = document.getElementById("main-t")
+    bluecolor.style.backgroundImage = "url('imgs/giphy.webp')"
+    bluecolor.style.backgroundPosition = "center"
+    bluecolor.style.backgroundSize ="cover"
+    bluecolor.style.transition = '3s'
+
+}
+function yellowbor(){
+    var yellowcolor = document.getElementById("main-t")
+    yellowcolor.style.backgroundImage = "url('imgs/giphy(1).webp')"
+    yellowcolor.style.backgroundPosition = "center"
+    yellowcolor.style.backgroundSize ="cover"
+    yellowcolor.style.transition = '3s'
+}
+// function bgchange2() {
+// //   const eeee = document.getElementById('gift_item_2')
+// //     eeee.style.transition = 'all 3s';
+// }
+// $("#get_item_2").click(function(){
+//     $(".trans").toggleClass(".gift_item_1_tr");
+// });
